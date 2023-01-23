3874ab7367576c748fe23bec5ee46f2566a1ad39
Andrew Zang <andrewz020529@gmail.com>
2023-01-23 17:08:12 -0500

    learned struct and array and dataLocation

--- 02-basics-of-solidity.sol
+++ 02-basics-of-solidity.sol
@@ -5,6 +5,8 @@ pragma solidity 0.8.8;
 // ^ means any version >= 0.8.7, e.g. ^0.8.7
 // between a range: >=0.8.7 <0.9.0
 
+
+
 contract SimpleStorage {
     // basic types: boolean, uint, int, address, bytes (8 bits -> 1 byte)
     // we can specify the bits for a uint variable e.g. uint256 
@@ -13,7 +15,48 @@ contract SimpleStorage {
     // max bytes is byte32, min int is int8
 
     uint256 favoriteNumber;
-    
-}
-// think of contract as a class
+    People public person = People( { favoriteNumber: 2, name: "Andrew" } );
+    // initialize an object
+    // People( { favoriteNumber: 2, name: "Andrew" } )
+    // People( "Andrew", 2 )
 
+    // class, struct
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people; // people is an array of People
+
+    
+    function store(uint256 _favoriteNumber ) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // view -> read only
+    // pure -> doesn't allow reading
+    // view and pure functions, when called along, don't spend gas
+    // they only spend gas if called by other contracts (functions)
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+
+    // data location must be "storage", "memory", or "calldata"
+    // or ( "stack", "code", "logs" )
+    // data location can only be specified for array, struct or mapping types
+    
+    // calldata and memory -> data will be stored temporarily
+    // calldata -> cannot modify data
+    // storage -> data exists outside of function executions
+    
+    // we cannot specify variables to be stack, code or logs
+    function addPerson( string memory _name, uint256 _favoriteNumber ) public {
+        people.push(People( _favoriteNumber, _name ));
+    }
+
+
+}
+
+
+// think of contract as a class
+// deployed Contract has an Address
