7d6d6883fefdab70571dc149a5c67b565e7d8465
Andrew Zang <andrewz020529@gmail.com>
2023-02-02 15:41:57 -0500

    learned constructor and modifier

--- 04-fund-me/FundMe.sol
+++ 04-fund-me/FundMe.sol
@@ -16,18 +16,49 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         // set a minimum amount in USD
 
         // global keyword to get the amount of money who calls this function
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough"); // 1e18 = 1 * 10^18 wei = 1 Eth
         funders.push( msg.sender );
-        addressToAmountFunded[ msg.sender ] = msg.value;
+        addressToAmountFunded[ msg.sender ] += msg.value;
         // require revert the action if the condition is not satisfied
         // second field is the error message send back
         // require revert all prior actions/computations
         
         // 18 decimal places
     }
-    // function withdraw() {}
+
+
+    function withdraw() public onlyOwner {
+        
+        
+        for( uint256 i = 0; i < funders.length; i++ ){
+            address funder = funders[ i ];
+            addressToAmountFunded[ funder ] = 0;
+        }
+        // reset the array
+        funders = new address[](0); // 0 objects to start
+        // withdraw funds
+
+        // 3 ways to send eth
+        // transfer
+        // send
+        // call
+
+        (bool callSuccess,) = payable(msg.sender).call{ value: address(this).balance }("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "You are not the owner!");
+        _; // call function code
+    }
 }
