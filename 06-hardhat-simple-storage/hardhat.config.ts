fa4f9b681a0fd2cc3ee3a1e10f4819e2cb370fba
Andrew Zang <andrewz020529@gmail.com>
2023-02-25 11:08:14 -0500

    coverage, gasReporter, lesson6 complete

--- 06-hardhat-simple-storage/hardhat.config.ts
+++ 06-hardhat-simple-storage/hardhat.config.ts
@@ -1,14 +1,18 @@
 import { HardhatUserConfig } from 'hardhat/config'
 
-import('@nomiclabs/hardhat-etherscan')
+import '@nomiclabs/hardhat-etherscan'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
 import '@nomicfoundation/hardhat-toolbox'
 import 'dotenv/config'
 import './tasks/block-number'
 
-const GEORLI_RPC_URL = process.env.GEORLI_RPC_URL
-const GEORLI_PRIVATE_KEY = process.env.GEORLI_PRIVATE_KEY
+const GEORLI_RPC_URL = process.env.GEORLI_RPC_URL || ''
+const GEORLI_PRIVATE_KEY = process.env.GEORLI_PRIVATE_KEY || ''
 
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
+
+const COIN_MARKET_CAP_API_KEY = process.env.COIN_MARKET_CAP_API_KEY || ''
 
 const config: HardhatUserConfig = {
 	defaultNetwork: 'hardhat',
@@ -26,6 +30,13 @@ const config: HardhatUserConfig = {
 	solidity: '0.8.7',
 	etherscan: {
 		apiKey: ETHERSCAN_API_KEY,
+	},
+	gasReporter: {
+		enabled: true, // switch to false if you don't want gas reporter
+		outputFile: 'gas-report.txt',
+		noColors: true,
+		currency: 'USD',
+		coinmarketcap: COIN_MARKET_CAP_API_KEY,
 	},
 }
 
